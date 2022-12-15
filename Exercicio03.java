02b57af907deac6468a7db7cf2b922b921e1e655
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-12-15 16:24:49 -0300

    Add files via upload

---
+++ Exercicio03.java
@@ -0,0 +1,23 @@
+//3) Escreva um algoritmo para ler dois valores do teclado e exibir como resposta a soma dos valores.
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+public static void main (String[]args){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Digite o Primeiro valor");
+
+int n1= sc.nextInt();
+
+System.out.println("Digite o Segundo valor que deseja somar");
+
+int n2= sc.nextInt();
+
+System.out.println(n1+n2);
+
+}}
+
+
